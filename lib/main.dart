a786616e75d01b89ce8c7f5e3e2b79f1d1a139c6
N4n3x <alex.hern.dev@gmail.com>
2021-05-12 15:22:45 +0200

    feat: integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:firebasexflutter/pages/home.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
