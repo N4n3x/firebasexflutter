07037c6bac0e42b4afc3daf6e77cf5b7d32371e1
N4n3x <alex.hern.dev@gmail.com>
2021-05-12 15:18:44 +0200

    feat: interation

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -6,8 +7,44 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool _initialized = false;
+  bool _error = false;
+
+  void initializeFlutterFire() async {
+    try {
+      // Wait for Firebase to initialize and set `_initialized` state to true
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch (e) {
+      // Set `_error` state to true if Firebase initialization fails
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    initializeFlutterFire();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (_error) {
+      return Container(
+        child: Text("erreur"),
+      );
+    }
+
+    if (!_initialized) {
+      return Container(
+        child: Text("wait"),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Firebase"),
